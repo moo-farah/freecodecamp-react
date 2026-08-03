bed9d3ec85a5f0e42c1fe2fabd634bafaf462659
moo-farah <rizmik.farah@gmail.com>
2026-08-03 17:55:42 +0300

    add: navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 const Navbar = () => {
   return (
-   <nav className="mt-2 text-xl">
-    <ul className="flex flex-row items-center justify-center gap-4" >
-        <li><a href="#">Company</a></li>
-        <li><a href="#"></a></li>
-        <li><a href="#">Careers</a></li>
-        <li><a href="#">Docs</a></li>
-        <li>
-            <button className="bg-zinc-600 hover:bg-zinc-900 text-white px-3 py-1 rounded-lg" aria-expanded>Sing up</button>
-        </li>
-    </ul>
-   </nav>
+   <nav className="bg-amber-400 px-4 py-5 mt-2">
+      <div className="flex flex-row items-center justify-around">
+        <ul className="flex flex-row items-center gap-4">
+          <li><a href="/company">Company</a></li>
+          <li><a href="/pricing">Pricing</a></li>
+          <li><a href="/careers">Careers</a></li>
+          <li><a href="/docs">Docs</a></li>
+        </ul>
+
+        <div className="flex flex-row items-center gap-5">
+          <button className="hover:bg-[#1D1D1F] text-white text-sm px-4 py-3 rounded-full font-medium" aria-expanded={false}>Sign in</button>
+          <button className="bg-[#1D1D1F] text-white text-sm px-4 py-3 rounded-full font-medium" aria-expanded={false}>Sign up</button>
+        </div>
+      </div>
+    </nav>
   )
 }
 
